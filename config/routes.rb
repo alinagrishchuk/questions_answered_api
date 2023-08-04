d810175d0db29d5f15f2af3b7ef9e97aa89a8ea8
Alina Hryshchuk <alina.hryshchuk.uk@gmail.com>
2023-08-04 08:35:08 +0100

    remove namespaces to avoid issues with API links()

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  resources :answers
-  scope :api do
-    scope :v1 do
-      jsonapi_resources :questions, only: [:index, :create, :show]
-      jsonapi_resources :answers, only: [:create]
-    end
-  end
+  jsonapi_resources :questions, only: [:index, :create, :show]
+  jsonapi_resources :answers, only: [:create]
 end
